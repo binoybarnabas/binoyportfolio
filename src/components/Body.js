f1882e1547c9a5ae3eb9c17964bf571973c481d4
binoy barnabas <binoybarnabas@gmail.com>
2022-05-04 23:56:13 +0530

    fixed the responsiveness of image and content

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,44 @@
 import React, { Component } from "react";
 import Image from "react-bootstrap/Image";
 import image from "../assets/binoy.jpg";
-import '../components/Header.css';
+import "../components/Header.css";
 import styled from "styled-components";
+import "../components/Header.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Body extends Component {
   render() {
-    const alignment = {
-      height: "400px",
-      width: "400px",
-      position: "absolute",
-      top: "70px",
-      left: "800px",
-    };
-
-    
-    return(
-        <div style={alignment} className="image-responsive">
-        <Image src={image}  fluid roundedCircle/>
-      </div></Responsive>
-      
+    return (
+      <Container fluid="true">
+        <Row>
+          <Col sm={7}>
+            <ContentResponser>content</ContentResponser>
+          </Col>
+          <Col sm={5}>
+            <ImageResponser>
+              <Image src={image} fluid roundedCircle />
+            </ImageResponser>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
 
 export default Body;
 
-
 //styled components code
+const ImageResponser = styled.div`
+  height: 400px;
+  width: 400px;
+  position: fixed;
+  top: 100px;
+  right: 200px;
+  max-width: 100%;
+`;
 
+const ContentResponser = styled.div`
+  position: fixed;
+  top: 100px;
+  left: 200px;
+`;
